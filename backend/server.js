048d097b4e0f45606c819ac64326deae10ace024
NinaAtanaskovic <nina.atanaskovic@gmail.com>
2026-06-13 03:52:23 +0200

    Backend: zavrsen

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 
 import connectDB from './config/db.js';
@@ -18,6 +19,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
